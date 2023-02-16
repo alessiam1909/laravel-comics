f3a7d3895f9eb317810a5a7777eabe4d2b74b3de
alessiam1909 <ale.menon.vr@gmail.com>
2023-02-16 20:10:25 +0100

    creazione foreach per stampare tutte le cards dei fumetti, e sistemazione array associativo con assegnaazione chiave valore

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $fumetti = config('db.comics');
+    return view('home', compact('fumetti'));
 });
