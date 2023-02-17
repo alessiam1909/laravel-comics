c0118cfa4e5a0643544105bc3074e072bfc7f30e
alessiam1909 <ale.menon.vr@gmail.com>
2023-02-17 09:56:05 +0100

    stilizzazione body con cards e jumbotron, riprendendo dal vecchio esercizio vite-comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,15 +15,22 @@
 
 <body>
     @include('partials.header')
+    <div class="jumbotron">
+    </div>
     <main class="bg-cards">
+        <div class="current-button">CURRENT SERIES</div>
         <div class="container-cards">
-            @foreach ($fumetti as $fumetto)
-                <div class="fumetto">
-                    <img class="img-fumetto" src="{{$fumetto['thumb']}}" alt="{{$fumetto['title']}}">
-                    <h3>{{$fumetto['title']}}</h3>
-                </div>
-                
-            @endforeach
+            <div class="row-fumetti">
+                @foreach ($fumetti as $fumetto)
+                    <div class="col-fumetto">
+                        <div class="contenitore-img">
+                            <img class="img-fumetto" src="{{$fumetto['thumb']}}" alt="{{$fumetto['title']}}">
+                        </div>
+                        <h3 class="title">{{$fumetto['title']}}</h3>
+                    </div> 
+                @endforeach
+            </div>
+            <button class="load-button">LOAD MORE</button>
         </div>
     
     </main>
