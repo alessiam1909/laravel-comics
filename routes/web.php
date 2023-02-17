74679024179c8f038e7eff54439961759fe4128e
alessiam1909 <ale.menon.vr@gmail.com>
2023-02-17 13:26:17 +0100

    creazione app.blade come layout base e creazione dello yield

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,70 @@ Route::get('/', function () {
     $fumetti = config('db.comics');
     return view('home', compact('fumetti'));
 });
+
+
+Route::get('/header', function(){
+    $nav =  [
+        [
+            "label" =>'CHARACTERS',
+            "url"=> '#',
+            "active"=> false
+
+        ],
+        [
+            "label" =>'COMICS',
+            "url"=> '#',
+            "active"=> true
+
+        ],
+        [
+            "label" =>'MOVIES',
+            "url"=> '#',
+            "active"=> false
+
+        ],
+        [
+            "label" =>'TV',
+            "url"=> '#',
+            "active"=> false
+
+        ],
+        [
+            "label" =>'GAMES',
+            "url"=> '#',
+            "active"=> false
+
+        ],
+        [
+            "label" =>'COLLECTIBLES',
+            "url"=> '#',
+            "active"=> false
+
+        ],
+        [
+            "label" =>'VIDEOS',
+            "url"=> '#',
+            "active"=> false
+
+        ],
+        [
+            "label" =>'FANS',
+            "url"=> '#',
+            "active"=> false
+
+        ],
+        [
+            "label" =>'NEWS',
+            "url"=> '#',
+            "active"=> false
+
+        ],
+        [
+            "label" =>'SHOP',
+            "url"=> '#',
+            "active"=> false
+
+        ]
+        ];
+        return view('header', compact('nav'));
+});
