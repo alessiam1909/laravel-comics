68994f168c8e652862cef788613ec30c737975c4
alessiam1909 <ale.menon.vr@gmail.com>
2023-02-17 13:32:43 +0100

    trasformazione del body della pagina in una section dello yield

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,24 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>DC Comics</title>
+@extends('layouts.app')
 
 
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    @include('partials.header')
-    <div class="jumbotron">
-    </div>
-    <main class="bg-cards">
-        <div class="current-button">CURRENT SERIES</div>
-        <div class="container-cards">
-            <div class="row-fumetti">
-                @foreach ($fumetti as $fumetto)
-                    <div class="col-fumetto">
-                        <div class="contenitore-img">
-                            <img class="img-fumetto" src="{{$fumetto['thumb']}}" alt="{{$fumetto['title']}}">
-                        </div>
-                        <h3 class="title">{{$fumetto['title']}}</h3>
-                    </div> 
-                @endforeach
-            </div>
-            <button class="load-button">LOAD MORE</button>
+@section('content')
+<div class="jumbotron">
+</div>
+<main class="bg-cards">
+    <div class="current-button">CURRENT SERIES</div>
+    <div class="container-cards">
+        <div class="row-fumetti">
+            @foreach ($fumetti as $fumetto)
+                <div class="col-fumetto">
+                    <div class="contenitore-img">
+                        <img class="img-fumetto" src="{{$fumetto['thumb']}}" alt="{{$fumetto['title']}}">
+                    </div>
+                    <h3 class="title">{{$fumetto['title']}}</h3>
+                </div> 
+            @endforeach
         </div>
-    
-    </main>
-    @include('partials.footer')
-</body>
+        <button class="load-button">LOAD MORE</button>
+    </div>
 
-</html>
+</main>
+@endsection
